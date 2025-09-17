0ab95c3a8bec96a84901a9d2cc67ed9796b5e1e8
grzlz <guillermo.rdzlpz@gmail.com>
2025-09-17 10:06:47 -0600

    Automatic deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,8 +19,8 @@
 		content="Transform your git history into actionable architectural insights. Import git log, export insights."
 	/>
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://vandeley.art" />
-	<meta property="og:image" content="https://vandeley.art/screenshot.png" />
+	<meta property="og:url" content="https://www.vandeley.art" />
+	<meta property="og:image" content="https://www.vandeley.art/screenshot.png?v=20240508" />
 	<meta property="og:image:alt" content="Vandeley Analytics - Git History Analysis Dashboard" />
 	<meta property="og:site_name" content="vandeley.art" />
 
@@ -31,7 +31,7 @@
 		name="twitter:description"
 		content="Transform your git history into actionable architectural insights. Advanced analytics for software architects."
 	/>
-	<meta name="twitter:image" content="https://vandeley.art/screenshot_tw.png" />
+	<meta name="twitter:image" content="https://www.vandeley.art/screenshot_tw.png?v=20240508" />
 	<meta name="twitter:image:alt" content="Vandeley Analytics - Git History Analysis Dashboard" />
 
 	<!-- Standard Meta Tags -->
